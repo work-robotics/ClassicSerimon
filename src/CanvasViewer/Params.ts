911ebb4f4698ee7d53ac8d5ce79c7ff8d1614749
AtsushiSaito <atsushi31100854@gmail.com>
2021-03-21 21:36:14 +0900

    不要なパラメータを削除

--- src/CanvasViewer/Params.ts
+++ src/CanvasViewer/Params.ts
@@ -1,8 +1,6 @@
 export class Params {
   public rowHeight: number;
   public fontSize: number;
-  public fontColor: string;
-  public backgroundColor: string;
   public selectCellColor: string;
   public fontFamily: string;
   public lineNumbersWidth: number;
@@ -14,8 +12,6 @@ export class Params {
   constructor() {
     this.rowHeight = 20;
     this.fontSize = 16;
-    this.fontColor = "#000";
-    this.backgroundColor = "#fff";
     this.selectCellColor = "#0000ff";
     this.fontFamily = "JetBrains Mono, Source Han Code JP, Menlo, Consolas";
     this.lineNumbersWidth = 90;
