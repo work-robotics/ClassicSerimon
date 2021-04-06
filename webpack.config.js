6f69405f9f5fb20a71e96cc187d426597460440b
AtsushiSaito <atsushi31100854@gmail.com>
2021-04-06 16:35:01 +0900

    開発時以外はホットリロードを実行しないように変更

--- webpack.config.js
+++ webpack.config.js
@@ -8,28 +8,34 @@ const { format } = require("util");
 class ElectronAutoRepload {
   server = undefined;
   intervalId = undefined;
+  useElectronConnect = undefined;
   mainProcessPath = "./dist/main.js";
   constructor() {
-    this.intervalId = setInterval(() => {
-      if (this.server == undefined && fs.existsSync(this.mainProcessPath)) {
-        this.server = electron.create();
-        this.server.start();
-        console.log("\n\u001b[36m===================================================\u001b[0m");
-        console.log(format("\u001b[36m[%s] ElectronServer Start.\u001b[0m", new Date()));
-        console.log("\u001b[36m===================================================\u001b[0m\n");
-        clearInterval(this.intervalId);
-      }
-    }, 1000);
+    this.useElectronConnect = process.env.APP_DEV ? process.env.APP_DEV.trim() == "true" : false;
+    if (this.useElectronConnect) {
+      this.intervalId = setInterval(() => {
+        if (this.server == undefined && fs.existsSync(this.mainProcessPath)) {
+          this.server = electron.create();
+          this.server.start();
+          console.log("\n\u001b[36m===================================================\u001b[0m");
+          console.log(format("\u001b[36m[%s] ElectronServer Start.\u001b[0m", new Date()));
+          console.log("\u001b[36m===================================================\u001b[0m\n");
+          clearInterval(this.intervalId);
+        }
+      }, 1000);
+    }
   }
   apply(compiler) {
-    compiler.hooks.done.tap("ElectronAutoRepload", () => {
-      if (this.server != undefined) {
-        this.server.reload();
-        console.log("\n\u001b[36m===================================================\u001b[0m");
-        console.log(format("\u001b[36m[%s] Renderer Reload.\u001b[0m", new Date()));
-        console.log("\u001b[36m===================================================\u001b[0m\n");
-      }
-    });
+    if (this.useElectronConnect) {
+      compiler.hooks.done.tap("ElectronAutoRepload", () => {
+        if (this.server != undefined) {
+          this.server.reload();
+          console.log("\n\u001b[36m===================================================\u001b[0m");
+          console.log(format("\u001b[36m[%s] Renderer Reload.\u001b[0m", new Date()));
+          console.log("\u001b[36m===================================================\u001b[0m\n");
+        }
+      });
+    }
   }
 }
 
